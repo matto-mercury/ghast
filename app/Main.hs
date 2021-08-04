57c0214fd249e6ec7dcd3ff3c2727d5b7fab6faa
Matt Olson <matto@mercury.com>
2021-08-04 09:17:38 -0700

    log headers

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Repl
 import Data.Aeson
+import Data.CaseInsensitive
 import Network.HTTP.Simple
 import qualified Data.ByteString.Char8 as B8
 
@@ -12,3 +13,6 @@ main = do
   resp <- httpBS authedReq
   let body = getResponseBody resp
   B8.putStrLn body
+  let headers = getResponseHeaders resp
+  let headerStr = foldMap (\(k,v) -> (original k) <> v <> (B8.pack "\n")) headers
+  B8.putStrLn headerStr
