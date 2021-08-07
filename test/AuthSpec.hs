23b7a5c199614b7f9b4781e2c94e0c80811848d4
Matt Olson <matto@mercury.com>
2021-08-06 17:51:05 -0700

    got a properly failing test

--- test/AuthSpec.hs
+++ test/AuthSpec.hs
@@ -4,21 +4,27 @@ import Control.Monad.Catch
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.CaseInsensitive as CI
 import Network.HTTP.Simple
+import Network.HTTP.Types.Header
 import Repl
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "Basic auth header generator" $ do
-    let userid = "user"
-    let passwd = "pass"
-    let authName = CI.mk $ B8.pack "Authorization"
-    it "generates a body string that starts with 'Basic '" $ do
-      body <- do
-        bareReq <- spuriousRequest
-        getRequestHeader authName $
-            authenticateWithBasic userid passwd bareReq
-      "Basic " `shouldBe` take 6 (B8.unpack body)
+    it "generates a value string that starts with 'Basic '" $ do
+      value <- do head . getRequestHeader authName
+       . authenticateWithBasic userid passwd
+       <$> spuriousRequest
+      "Basic " `shouldBe` take 6 (B8.unpack value)
 
 spuriousRequest :: MonadThrow m => m Request
 spuriousRequest = parseRequest "https://foo.bar"
+
+userid :: String
+userid = "user"
+
+passwd :: String
+passwd = "pass"
+
+authName :: HeaderName
+authName = CI.mk $ B8.pack "Authorization"
