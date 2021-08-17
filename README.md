50cbea7710f857142e1bbfe6fe1ff0b3a5795451
Matt Olson <matto@mercury.com>
2021-08-16 22:25:18 -0700

    URI-encoded fragments and wrote some tests

--- README.md
+++ README.md
@@ -6,22 +6,24 @@ GitHub Actions STatus CLI tool
 - got basic auth working
 - hit Actions "list runs" endpoint
 - some helpers around request builders and parameters
-
-## Next up
-
 - figure out what info I want to pull out of Actions
 - get latest Jobs based on top Run
 - build a data structure for those too
+- URL-encode parameter values
+
+## Next up
+- get latest run, jobs in current branch
+- list failed jobs
+- ReaderT for credentials
 
 ## Medium term
 - pull repository info
   - owner (e.g. MercuryTechnologies)
   - repo (e.g. mercury-web-backend)
   - current branch (e.g. matto/rul-79)
-- URL-encode parameter values
-- get latest run, jobs in current branch
-- list failed jobs
 - download logs from failed jobs
+- figure out how to work nicely with String and Text
+ - similar for datetimes
 
 ## Longer term
 - oauth2 support
---
+++ test/UriFragmentSpec.hs
@@ -0,0 +1,38 @@
+module UriFragmentSpec where
+
+import Control.Monad.Catch
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.CaseInsensitive as CI
+import Network.HTTP.Simple
+import Network.HTTP.Types.Header
+import Repl
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Basic percent-encoding on rendered URI fragments" $ do
+    it "does nothing when input doesn't need encoding" $
+      render "hello" `shouldBe` "hello"
+
+    it "percent-encodes spaces and ascii punctuation" $
+      render "hi& there" `shouldBe` "hi%26%20there"
+
+    it "percent-encodes multibyte characters" $
+      render "hi günter" `shouldBe` "hi%20g%C3%BCnter"
+
+  describe "Key-value parameters get rendered with a = between them" $ do
+    it "does what it says on the tin" $
+      render (Param "foo" "bar") `shouldBe` "foo=bar"
+
+    it "plays nice with URI encoding" $
+      render (Param "name" "günter") `shouldBe` "name=g%C3%BCnter"
+
+  describe "Lists of key-value parameters get encoded properly" $ do
+    it "successfully renders nothing for an empty list" $
+      render ([] @Parameter) `shouldBe` ""
+
+    it "renders a single parameter without fuss" $
+      render [Param "key" "val"] `shouldBe` "?key=val"
+
+    it "renders several parameters with intercalated &s" $
+      render [Param "key" "val", Param "foo" "bar"] `shouldBe` "?key=val&foo=bar"
